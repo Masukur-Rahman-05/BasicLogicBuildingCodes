fd8d263b7d10f1466a5ece4b7075ada515d19d2a
Masuk <masukrahoman5@gmail.com>
2026-03-24 12:27:09 +0600

    BruteForce solution of Finding LCM in problem 9

---
+++ PythonCodes/problem9.py
@@ -0,0 +1,26 @@
+# Given two positive integers a and b. Find the Least Common Multiple (LCM) of a and b.
+# LCM of two numbers is the smallest number which can be divided by both numbers. 
+
+#     Input :  a = 10, b = 5
+#     Output :  10
+#     Explanation : 10 is the smallest number divisible by both 10 and 5
+
+#     Input :  a = 5, b = 11
+#     Output :  55
+#     Explanation : 55 is the smallest number divisible by both 5 and 11
+
+
+#...................................................Solution 1...................................................
+# def find_lcm(a,b):
+
+#     result = max(a,b)
+
+#     while True:
+#         if result % a == 0 and result % b == 0:
+#             return result
+#         result += 1
+
+# if __name__ == "__main__":
+#     print(find_lcm(12,18))
+
+#.....................................................Solution  2...................................................
