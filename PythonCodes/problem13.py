e8468f4c464c14866c2b7404ae2d09d5a013467b
Masuk <masukrahoman5@gmail.com>
2026-03-26 21:03:22 +0600

    Solve the Triangular Number problem

---
+++ PythonCodes/problem13.py
@@ -0,0 +1,26 @@
+# Find n-th term of series 1, 3, 6, 10, 15, 21...
+# Last Updated : 21 Mar, 2025
+
+# Given a number n, find the n-th term in the series 1, 3, 6, 10, 15, 21...
+
+# Examples
+
+#     Input  3
+#     Output 6
+
+#     Input  4
+#     Output  10
+
+# The given series represent triangular numbers which are sums of natural numbers.
+
+def find_nth_term(n):
+
+    result = 0
+
+    for i in range(1,n+1):
+        result = result + i
+
+    return result
+
+if __name__=="__main__":
+    print(find_nth_term(4))
