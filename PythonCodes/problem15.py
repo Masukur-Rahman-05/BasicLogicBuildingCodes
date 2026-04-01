fd48b28a711fb93f8cbd90a3cf99699679c6e494
Masuk <masukrahoman5@gmail.com>
2026-04-01 13:17:00 +0600

    solve: solve the Permutation calculation in O(r) time

--- PythonCodes/problem15.py
+++ PythonCodes/problem15.py
@@ -13,14 +13,48 @@
 #     Output: 120
 #     Explanation: 6P3 = 6! / (6 - 3)!  = 120
 
+# nPr= n! / (n-r)!
+
 #.........................................................Solution 1....................................................
-import math
+# import math
+
+# def permutation(n,r):
+
+#     if r > n:
+#         return 0
+    
+#     return math.factorial(n) // math.factorial(n-r)
+
+# print(permutation(6,3))
+
+
+#............................................................Solution 2 (Optimized O(r))....................................................................
+
+# Instead of computing full factorials, multiply only required terms.
+
+#                                   nPr=n×(n−1)×...×(n−r+1)
+
+# def permutation(n,r):
+#     if r>n:
+#         return 0
+    
+#     res = 1
+#     for i in range(r):
+#         res *= (n-i)
+#     return res
+
+# print(permutation(6,3))
+
+#........................................more Optimized solution.....................................................................
 
 def permutation(n,r):
-
-    if r > n:
+    if r>n:
         return 0
     
-    return math.factorial(n) // math.factorial(n-r)
+    r = min(r,n-r)
+    res = 1
+    for i in range(r):
+        res *= (n-i)
+    return res
 
 print(permutation(6,3))
